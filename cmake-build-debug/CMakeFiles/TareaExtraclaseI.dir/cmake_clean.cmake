1c1da1e4cd8caa4fe059af69b03b2bf02b43e152
Anthony <antoca29@gmail.com>
2021-03-04 01:25:48 -0600

    First Commit from linux

---
+++ cmake-build-debug/CMakeFiles/TareaExtraclaseI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TareaExtraclaseI.dir/main.cpp.o"
+  "TareaExtraclaseI"
+  "TareaExtraclaseI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaExtraclaseI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
